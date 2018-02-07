1e9731c18a9ddb87a4efb767c7f2c3456c627c4e
Yuhuang Hu <duguyue100@gmail.com>
2018-02-07 15:11:38 +0100

    update calibration

---
+++ src/controller/cfg/config_car_uri.sh
@@ -0,0 +1,7 @@
+# car is master
+export ROS_MASTER_URI=http://172.19.8.123:11311
+export ROS_HOSTNAME=172.19.8.123
+
+# car is slave
+# export ROS_MASTER_URI=http://172.19.11.178:11311
+# export ROS_HOSTNAME=172.19.11.178
---
+++ src/controller/cfg/config_host_uri.sh
@@ -0,0 +1,7 @@
+# host is master
+# export ROS_MASTER_URI=http://172.19.11.178:11311
+# export ROS_HOSTNAME=172.19.11.178
+
+# host is slave
+export ROS_MASTER_URI=http://172.19.8.123:11311
+export ROS_HOSTNAME=172.19.11.178
