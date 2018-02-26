a9b0c665153a6f2e18b41c7a548e321f3c3e2e5c
Yuhuang Hu <duguyue100@gmail.com>
2018-02-26 13:04:28 +0100

    clean up tooling

--- tools/keras_to_tf.py
+++ tools/keras_to_tf.py
@@ -10,9 +10,6 @@ from __future__ import print_function
 import argparse
 
 import tensorflow as tf
-from tensorflow.python.saved_model import builder as saved_model_builder
-from tensorflow.python.saved_model import tag_constants, \
-    signature_constants
 from tensorflow.python.framework import graph_util
 from tensorflow.python.framework import graph_io
 import keras.backend as K
@@ -54,33 +51,6 @@ def keras2tf(in_model, in_model_mode, out_model):
         constant_graph, ".", out_model, as_text=False)
     print('saved the freezed graph (ready for inference) at: ',
           out_model)
-
-    #  prediction_signature = \
-    #      tf.saved_model.signature_def_utils.predict_signature_def(
-    #          {"input": keras_model.input},
-    #          {"prediction": keras_model.output})
-    #
-    #  builder = saved_model_builder.SavedModelBuilder(out_model)
-    #  legacy_init_op = tf.group(
-    #      tf.tables_initializer(), name='legacy_init_op')
-    #
-    #  init_op = tf.group(
-    #      tf.global_variables_initializer(),
-    #      tf.local_variables_initializer())
-    #
-    #  sess.run(init_op)
-    #
-    #  # Add the meta_graph and the variables to the builder
-    #  builder.add_meta_graph_and_variables(
-    #      sess, [tag_constants.SERVING],
-    #      signature_def_map={
-    #          signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY:
-    #              prediction_signature,
-    #      },
-    #      legacy_init_op=legacy_init_op)
-    #
-    #  # save the graph
-    #  builder.save()
 
 
 if __name__ == '__main__':
