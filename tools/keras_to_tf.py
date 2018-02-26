5035ecc0893da059f185c04feee38bff219efa40
Yuhuang Hu <duguyue100@gmail.com>
2018-02-26 13:03:47 +0100

    update saved model

--- tools/keras_to_tf.py
+++ tools/keras_to_tf.py
@@ -13,6 +13,8 @@ import tensorflow as tf
 from tensorflow.python.saved_model import builder as saved_model_builder
 from tensorflow.python.saved_model import tag_constants, \
     signature_constants
+from tensorflow.python.framework import graph_util
+from tensorflow.python.framework import graph_io
 import keras.backend as K
 from keras.backend import tf as ktf
 from keras.models import load_model
@@ -37,32 +39,48 @@ def keras2tf(in_model, in_model_mode, out_model):
         pre_trained_weights = in_model.replace('json', 'h5')
         keras_model.load_weights(pre_trained_weights)
 
-    prediction_signature = \
-        tf.saved_model.signature_def_utils.predict_signature_def(
-            {"input": keras_model.input},
-            {"prediction": keras_model.output})
+    num_output = 1
+    pred = [None]*num_output
+    pred_node_names = [None]*num_output
+    for i in range(num_output):
+        pred_node_names[i] = "prediction"+str(i)
+        pred[i] = tf.identity(
+            keras_model.outputs[i], name=pred_node_names[i])
+    print('output nodes names are: ', pred_node_names)
 
-    builder = saved_model_builder.SavedModelBuilder(out_model)
-    legacy_init_op = tf.group(
-        tf.tables_initializer(), name='legacy_init_op')
+    constant_graph = graph_util.convert_variables_to_constants(
+        sess, sess.graph.as_graph_def(), pred_node_names)
+    graph_io.write_graph(
+        constant_graph, ".", out_model, as_text=False)
+    print('saved the freezed graph (ready for inference) at: ',
+          out_model)
 
-    init_op = tf.group(
-        tf.global_variables_initializer(),
-        tf.local_variables_initializer())
-
-    sess.run(init_op)
-
-    # Add the meta_graph and the variables to the builder
-    builder.add_meta_graph_and_variables(
-        sess, [tag_constants.SERVING],
-        signature_def_map={
-            signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY:
-                prediction_signature,
-        },
-        legacy_init_op=legacy_init_op)
-
-    # save the graph
-    builder.save()
+    #  prediction_signature = \
+    #      tf.saved_model.signature_def_utils.predict_signature_def(
+    #          {"input": keras_model.input},
+    #          {"prediction": keras_model.output})
+    #
+    #  builder = saved_model_builder.SavedModelBuilder(out_model)
+    #  legacy_init_op = tf.group(
+    #      tf.tables_initializer(), name='legacy_init_op')
+    #
+    #  init_op = tf.group(
+    #      tf.global_variables_initializer(),
+    #      tf.local_variables_initializer())
+    #
+    #  sess.run(init_op)
+    #
+    #  # Add the meta_graph and the variables to the builder
+    #  builder.add_meta_graph_and_variables(
+    #      sess, [tag_constants.SERVING],
+    #      signature_def_map={
+    #          signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY:
+    #              prediction_signature,
+    #      },
+    #      legacy_init_op=legacy_init_op)
+    #
+    #  # save the graph
+    #  builder.save()
 
 
 if __name__ == '__main__':
